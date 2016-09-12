22b22da2a21e5b01293a88d9733ba7414c398dc7
daijiya <daijiya1314010301@163.com>
2016-09-12 09:15:49 +0800

    添加index.php页面

---
+++ application/views/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <base href="<?php echo site_url() ?>"
+    <script src="js/jquery-1.12.2.js"></script>
+    <title>首页</title>
+</head>
+<body>
+    haha
+</body>
+</html>
